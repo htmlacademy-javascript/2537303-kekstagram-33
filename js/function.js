3bfb53af25b2b0be564f6457c9fdc3ec3fdbb8b9
Dmitriy <dimanich12512@gmail.com>
2024-10-23 00:59:26 +1000

    Добавил Функции по дз

---
+++ js/function.js
@@ -0,0 +1,43 @@
+let lengthString = function(inputString, maxLength){
+  if (inputString.length > maxLength){
+    console.log('Длина строки больше ' + maxLength);
+  } else if(inputString.length < maxLength){
+    console.log('Длина строки меньше ' + maxLength);
+  }else{
+    console.log('Длина строки равна ' + maxLength)
+  }
+}
+
+lengthString('Прив', 5)
+
+
+let checkPalindrom = function(inputString){
+  let str = inputString.replaceAll(' ', '').toUpperCase();
+  let strReverse = '';
+  for(let i = str.length - 1; i >= 0; i--){
+    strReverse += str.charAt(i);
+  }
+  return str === strReverse;
+}
+
+console.log(checkPalindrom('оппо'));
+
+let checkNumber = function(inputString){
+  let str = inputString;
+  let digits = '';
+  for(i = 0; i <= str.length; i++ ){
+    let num = str[i];
+    let verifiedNumber = parseInt(num);
+
+    if(!Number.isNaN(verifiedNumber)){
+      digits += num;
+    }
+
+    if(digits === ''){
+      return NaN;
+    }
+  }
+  return parseInt(digits);
+}
+
+console.log(checkNumber('123ggg34'));
