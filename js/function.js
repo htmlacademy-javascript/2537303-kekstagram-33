3ed03c615f1bb4588f2de6c21a2e2bcc451e49c1
Dmitriy <dimanich12512@gmail.com>
2024-10-28 10:42:56 +1000

    Исправил ошибки

--- js/function.js
+++ js/function.js
@@ -1,40 +1,32 @@
-let lengthString = function(inputString, maxLength){
-  if (inputString.length > maxLength){
-    console.log('Длина строки больше ' + maxLength);
-  } else if(inputString.length < maxLength){
-    console.log('Длина строки меньше ' + maxLength);
-  }else{
-    console.log('Длина строки равна ' + maxLength)
-  }
-}
+const calculateLengthString = function(inputString, maxLength){
+  return inputString.length <= maxLength;
+};
 
-lengthString('Прив', 5)
+calculateLengthString('Привffff', 5);
 
 
-let checkPalindrom = function(inputString){
-  let str = inputString.replaceAll(' ', '').toUpperCase();
+const checkPalindrom = function(inputString){
+  const str = inputString.replaceAll(' ', '').toUpperCase();
   let strReverse = '';
   for(let i = str.length - 1; i >= 0; i--){
     strReverse += str.charAt(i);
   }
   return str === strReverse;
-}
+};
 
-console.log(checkPalindrom('оппо'));
+checkPalindrom('оппо');
 
-let checkNumber = function(inputString){
-  let str = inputString;
+const checkNumber = function(inputString){
   let digits = '';
-  for(i = 0; i <= str.length; i++ ){
-    let num = str[i];
-    let verifiedNumber = parseInt(num);
+  for(let i = 0; i <= inputString.length; i++){
+    const num = inputString[i];
+    const verifiedNumber = parseInt(num, 10);
 
     if(!Number.isNaN(verifiedNumber)){
       digits += num;
     }
-    }
   }
-  return parseInt(digits);
-}
+  return digits;
+};
 
-console.log(checkNumber('123ggg34'));
+checkNumber('123ggg34');
